70da3594eb93e5e17f5a04cc57dbcc3fb808f491
Fabian Merchan <fabian.merchan@streetshares.com>
2021-02-01 21:56:04 -0500

    added scss support for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../app/**/*.stories.mdx",
+    "../app/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
   ]
 }
--- app/components/Layout/Layout.jsx
+++ app/components/Layout/Layout.jsx
@@ -1,6 +1,8 @@
+import styles from './Layout.module.scss';
+
 const Layout = ({children}) => {
   return (
-    <div>
+    <div className={styles.Layout}>
       {children}
     </div>
   )
