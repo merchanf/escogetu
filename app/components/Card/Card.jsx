53ab495bc71807bca2a4dc394fab93cc5ca78959
Fabian Merchan <fabian.merchan@streetshares.com>
2021-02-03 19:20:04 -0500

    creating card module

--- app/components/Card/Card.jsx
+++ app/components/Card/Card.jsx
@@ -1,32 +1,65 @@
+import { useState } from "react";
 import TinderCard from "react-tinder-card";
 
-import styles from './Card.module.scss';
+import styles from "./Card.module.scss";
 
-const Card = ({list}) => {
-  const onSwipe = (direction) => {
-    console.log("You swiped: " + direction);
+const Card = ({ name, distance, pictures, onSwipe, index }) => {
+  const [selectedPicture, setSelectedPicture] = useState(0);
+
+  const handleRightButton = () => {
+    if (selectedPicture + 1 < pictures.length)
+      setSelectedPicture((prevState) => prevState + 1);
+    console.log("clicked right");
   };
 
-  const onCardLeftScreen = (myIdentifier) => {
-    console.log(myIdentifier + " left the screen");
+  const handleLeftButton = () => {
+    if (selectedPicture - 1 >= 0)
+      setSelectedPicture((prevState) => prevState - 1);
+    console.log("clicked left");
   };
-
-  const swiped = (direction, nameToDelete) => {
-    console.log('removing: ' + nameToDelete)
-  }
-
-  const outOfFrame = (name) => {
-    console.log(name + ' left the screen!')
-  }
 
   return (
-    <div className={styles.cardContainer}>
-      {list.map((character) =>
-        <TinderCard className={styles.swipe} key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-          <img className={styles.card} src={`https://via.placeholder.com/300x465?text=${character.name}`} />
-        </TinderCard>
-      )}
+    <div style={{
+      position: 'absolute',
+      zIndex: 10 - index
+    }}>
+      <TinderCard
+        className={styles.Container}
+        onSwipe={(dir) => onSwipe(dir, name)}
+      >
+        <div className={styles.Wrapper}>
+          <img
+            className={styles.Wrapper__Card}
+            src={pictures[selectedPicture]}
+            alt={`${name} - ${selectedPicture}`}
+          />
+          <p className={styles.Wrapper__Name}>
+            <b>{name}</b>, {distance}
+          </p>
+          <div className={styles.Wrapper__Buttons}>
+            {selectedPicture - 1 >= 0 ? (
+              <button
+                className={styles.Wrapper__Buttons__Button}
+                onClick={handleLeftButton}
+              >
+                {"<"}
+              </button>
+            ) : (
+              <div></div>
+            )}
+            {selectedPicture + 1 < pictures.length && (
+              <button
+                className={styles.Wrapper__Buttons__Button}
+                onClick={handleRightButton}
+              >
+                {">"}
+              </button>
+            )}
+          </div>
+        </div>
+      </TinderCard>
     </div>
+    
   );
 };
 
--- app/components/Card/Card.stories.js
+++ app/components/Card/Card.stories.js
@@ -1,33 +1,22 @@
-import Card from "./Card";
+import Card from './Card';
 
 export default {
   component: Card,
   title: 'Card',
 };
 
-const db = [
-  {
-    name: 'Richard Hendricks',
-    url: './img/richard.jpg'
-  },
-  {
-    name: 'Erlich Bachman',
-    url: './img/erlich.jpg'
-  },
-  {
-    name: 'Monica Hall',
-    url: './img/monica.jpg'
-  },
-  {
-    name: 'Jared Dunn',
-    url: './img/jared.jpg'
-  },
-  {
-    name: 'Dinesh Chugtai',
-    url: './img/dinesh.jpg'
-  }
+const onSwipe = (direction, nameToDelete) => {
+  console.log('removing: ' + nameToDelete)
+}
+
+const pictures = [
+  'https://via.placeholder.com/300x465/9B59B6?text=El%20Corral',
+  'https://via.placeholder.com/300x465/2E86C1?text=El%20Corral',
+  'https://via.placeholder.com/300x465/1ABC9C?text=El%20Corral',
+  'https://via.placeholder.com/300x465/F1C40F?text=El%20Corral',
+  'https://via.placeholder.com/300x465/7F8C8D?text=El%20Corral',
 ]
 
 export const Default = () => (
-  <Card list={db}/>
+  <Card name="El corral" distance="3 Km" pictures={pictures} onSwipe={onSwipe}/>
 );
