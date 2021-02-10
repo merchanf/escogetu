847cf4cf6c8d6842769df7d0b5cac75a1b65188a
Fabian Merchan <fabian.merchan@streetshares.com>
2021-02-09 22:26:37 -0500

    making first requests and adding them to the cardList

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import { createRef, useState, useMemo, useRef } from "react";
+import { createRef, useState, useMemo, useRef, useEffect } from "react";
+import useAxios from "axios-hooks";
+import axios from "axios";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-
 import Layout from "../app/components/Layout/Layout";
 import CardList from "../app/components/CardList/CardList";
 import {
@@ -12,13 +13,25 @@ import {
 } from "../app/components/Icons/Icons";
 import { db } from "../app/components/CardList/CardList.stories";
 
+const gMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+const defaultRadius = 2500;
 const LikeIconButton = withIconButton(LikeIcon);
 const ShareIconButton = withIconButton(ShareIcon);
 const CrossIconButton = withIconButton(CrossIcon);
-const alreadyRemoved = [];
+
+const nearbyRestaurantsEndpoint = (lat, long, radius, key) =>
+  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${radius}&type=restaurant&key=${key}`;
+
+const placePhotoSrc = (photoreference, maxheight = 16000) =>
+  `https://maps.googleapis.com/maps/api/place/photo?key=${gMapsApiKey}&photoreference=${photoreference}&maxheight=${1600}`;
+
+const getPlaceDetailsEndpoint = (placeId) =>
+  `https://maps.googleapis.com/maps/api/place/details/json?key=${gMapsApiKey}&place_id=${placeId}`;
 
 export default function Home() {
   const card = useRef(0);
+  const [loading, setLoading] = useState(true);
+  const [list, setList] = useState();
   const childRefs = useMemo(
     () =>
       Array(db.length)
@@ -27,13 +40,49 @@ export default function Home() {
     []
   );
 
+  useEffect(() => {
+    setLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        const {
+          data: { results },
+        } = await axios.get(
+          nearbyRestaurantsEndpoint(lat, long, defaultRadius, gMapsApiKey)
+        );
+        //console.log(results);
+        const db = results
+          .map(({ place_id, name, photos, geometry: { lat, lng } }) => {
+            if (!photos) return null;
+            const response = axios
+              .get(getPlaceDetailsEndpoint(place_id))
+              .then(({ data: { result: { photos } } }) => console.log(photos));
+            return {
+              name: name,
+              distance: "3 Km",
+              pictures: photos.map(({ photo_reference, height }) =>
+                placePhotoSrc(photo_reference, height)
+              ),
+              ref: createRef(),
+            };
+          })
+          .filter((result) => result != null);
+        console.log(db);
+        //setList(db);
+        //setLoading(false);
+      },
+      () => console.log("Denied")
+    );
+  }, []);
+
   const swiped = (direction, name) => {
-    console.log(direction, name)
+    console.log(direction, name);
     card.current += 1;
-  }
+  };
 
   const swipe = (dir) => {
-    childRefs[card.current].current.swipe(dir); // Swipe the card!
+    list[card.current].ref.current.swipe(dir); // Swipe the card!
   };
 
   return (
@@ -50,16 +99,20 @@ export default function Home() {
 
       <Layout>
         <div className={styles.Home}>
-          <CardList list={db} refs={childRefs} onSwipe={swiped} />
+          {loading ? (
+            <h3>Loading ... </h3>
+          ) : (
+            <CardList list={list} refs={childRefs} onSwipe={swiped} />
+          )}
           <div className={styles.Home__Buttons}>
             <CrossIconButton
-              onClick={() => swipe('left')}
+              onClick={() => swipe("left")}
               size="large"
               color="red"
             />
             <ShareIconButton color="blue" />
             <LikeIconButton
-              onClick={() => swipe('right')}
+              onClick={() => swipe("right")}
               size="large"
               color="green"
             />
