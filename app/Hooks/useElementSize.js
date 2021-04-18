3a3feaad19f90ba47977b72bd4c994ce2b1cc0e5
Fabian Merchan <fabian.merchan@yello.co>
2021-04-18 17:31:33 -0500

    removing console statements

--- app/Hooks/useElementSize.js
+++ app/Hooks/useElementSize.js
@@ -4,8 +4,6 @@ const useElementSize = (ref) => {
   const [size, setSize] = useState([0, 0]);
 
   useEffect(() => {
-    console.log(ref?.current);
-    console.log(ref?.current?.innerWidth);
     setSize([ref?.current?.innerWidth, ref?.current?.innerHeight]);
   }, [ref?.current?.innerWidth, ref?.current?.innerHeight]);
 
