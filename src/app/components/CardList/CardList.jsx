5fdaf628324620e5d6eacaf48b38e9ccde41cd99
stiven <stiven.avila@adroll.com>
2021-04-24 23:06:26 -0500

    Fix Linters

--- src/app/components/CardList/CardList.jsx
+++ src/app/components/CardList/CardList.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from '../Card/Card';
 
@@ -6,13 +6,6 @@ import styles from './CardList.module.scss';
 
 export const CardList = ({ list, onSwipe, onCardLeftScreen }) => {
   const ref = useRef(null);
-  const [width, setWidth] = useState();
-  const [height, setHeight] = useState();
-
-  useEffect(() => {
-    setWidth(ref?.current?.clientWidth);
-    setHeight(ref?.current?.clientHeight);
-  }, [ref]);
 
   return (
     <div className={styles.Container} ref={ref}>
