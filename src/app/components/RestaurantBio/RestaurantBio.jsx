c9a51f6cbf1f0cf8447f4fbc232347c823f937ed
LittePeter <57241172+LittePeter@users.noreply.github.com>
2021-04-30 19:39:52 -0500

    Feature/profile bio (#26)
    * -added info button
    
    * -added Card Bio
    
    * -added Card Bio

---
+++ src/app/components/RestaurantBio/RestaurantBio.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import styles from './RestaurantBio.module.scss';
+
+export const RestaurantBio = ({ name, open, setOpen }) => {
+  return (
+    <>
+      {open ? (
+        <div className={styles.Bio}>
+          <div className={styles.Bio__button}>
+            <button
+              type="button"
+              onClick={() => {
+                setOpen(false);
+              }}
+              className={styles.Bio__Button__Button}
+            >
+              X
+            </button>
+          </div>
+          <h2 className={styles.Bio__Name}>{name}</h2>
+        </div>
+      ) : null}
+    </>
+  );
+};
+RestaurantBio.propTypes = {
+  name: PropTypes.string.isRequired,
+  open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
+};
